403762ba0d082ee383f13c03c1e1d4106397b82f
one1erp <avigaile@one1.co.il>
2024-04-09 09:10:57 +0300

    Initial commit with additional files

---
+++ XmlService/BaseXmlHandler.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Common;
+using DAL;
+using LSSERVICEPROVIDERLib;
+using MSXML;
+
+namespace XmlService
+{
+    public abstract class BaseXmlHandler
+    {
+        protected DOMDocument objDoc;
+        protected DOMDocument objRes;
+        private readonly INautilusProcessXML _processXml;
+        private readonly string path;
+        private bool _succes;
+        private string _response;
+        private string fileName = "";
+
+
+        protected BaseXmlHandler(INautilusServiceProvider sp)
+        {
+
+            //Get xml processor
+            _processXml = Utils.GetXmlProcessor(sp);
+            //Get nautilus user
+            var ntlsUser = Utils.GetNautilusUser(sp);
+            //Get workstation of current user
+            var workstationId = ntlsUser.GetWorkstationId();
+            try
+            {
+
+
+            }
+            catch (Exception exception)
+            {
+
+                Logger.WriteLogFile(exception);
+            }
+
+        }
+        protected BaseXmlHandler(INautilusServiceProvider sp, string fileName)
+            : this(sp)
+        {
+            this.fileName = fileName;
+        }
+        /// <summary>
+        /// Run xml
+        /// </summary>
+        /// <returns>Success</returns>
+        /// 
+
+        public bool ProcssXml()
+        {
+            objRes = new DOMDocument();
+            _response = _processXml.ProcessXMLWithResponse(objDoc, objRes);
+            // _response = _processXml.ProcessXML(objDoc);
+            string suffix = ".xml";
+            string savePath = null;
+            try
+            {
+                var directoryPath = GetDirectoryPath();
+                if (directoryPath != null)
+                {
+                    //     for test
+                    //    objDoc.save(@"C:\temp\" + "Doc" + (DateTime.Now.ToString().MakeSafeFilename('_')) + ".xml");
+                    //    objRes.save(@"C:\temp\" + "Res" + (DateTime.Now.ToString().MakeSafeFilename('_')) + ".xml");
+
+                    string ut = DateTime.UtcNow.ToString("yyyy-MM-dd HH-mm-ss-fff");
+
+
+                    objDoc.save(@"C:\bb.xml");
+                    savePath = (@"C:\bbErr.xml");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex);
+                // אם הוא לא מצליח לשמור 
+            }
+
+            _succes = _response.Length == 0;// CheckErrors(objRes);
+            try
+            {
+                //שמירה של מסמך התשובה
+                if (!_succes && !string.IsNullOrEmpty(savePath))
+                    objRes.save(savePath + "_ERROR.xml");
+                else
+                    objRes.save(savePath + suffix);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Logger.WriteLogFile(ex);
+                // אם הוא לא מצליח לשמור 
+            }
+
+            return _succes;
+        }
+
+        public bool ProcssXmlWithOutResponse()
+        {
+            objRes = new DOMDocument();
+            //_response = _processXml.ProcessXMLWithResponse(objDoc, objRes);
+            _response = _processXml.ProcessXML(objDoc);
+
+            try
+            {
+                var directoryPath = GetDirectoryPath();
+                if (directoryPath != null)
+                {
+                    //     for test
+                    //    objDoc.save(@"C:\temp\" + "Doc" + (DateTime.Now.ToString().MakeSafeFilename('_')) + ".xml");
+                    //    objRes.save(@"C:\temp\" + "Res" + (DateTime.Now.ToString().MakeSafeFilename('_')) + ".xml");
+
+                    string ut = DateTime.UtcNow.ToString("yyyy-MM-dd HH-mm-ss-fff");
+
+
+                //    objDoc.save(directoryPath + "Doc_" + fileName.MakeSafeFilename('_') + (ut) + ".xml");
+                 //   objRes.save(directoryPath + "Res_" + fileName.MakeSafeFilename('_') + (ut) + ".xml");
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex);
+                // אם הוא לא מצליח לשמור 
+            }
+
+            _succes = _response.Length == 0;// CheckErrors(objRes);
+
+            return _succes;
+        }
+
+        public bool ProcssXmlWithOutSave()
+        {
+            objRes = new DOMDocument();
+            _response = _processXml.ProcessXMLWithResponse(objDoc, objRes);
+
+            _succes = _response.Length == 0;// CheckErrors(objRes);
+
+            return _succes;
+        }
+        /// <summary>
+        /// Get directory to save docs
+        /// </summary>
+        /// <returns></returns>
+        private string GetDirectoryPath()
+        {
+
+
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+
+
+            return path;
+        }
+
+        private bool CheckErrors(DOMDocument objRes)
+        {
+            try
+            {
+                IXMLDOMNode answer = objRes.getElementsByTagName("errors")[0];
+                return answer == null;
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Get response from xml by tag
+        /// </summary>
+        /// <param name="tagName">tag with response data</param>
+        /// <returns></returns>
+        public string GetValueByTagName(string tagName)
+        {
+            try
+            {
+
+                if (objRes != null)
+                {
+                    var newValue = ((dynamic)objRes.getElementsByTagName(tagName)[0]).nodeTypedValue;
+                    return newValue != null ? newValue.ToString() : null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex);
+
+            }
+            return null;
+
+        }
+
+        /// <summary>
+        /// Get response from xml by tag
+        /// </summary>
+        /// <param name="tagName">tag with response data</param>
+        /// <param name="index">Location of tag in response</param>
+        /// <returns></returns>
+        public object GetValueByTagName(string tagName, int index)
+        {
+            try
+            {
+
+                if (objRes != null)
+                {
+                    var newValue = ((dynamic)objRes.getElementsByTagName(tagName)[index]).nodeTypedValue;
+                    return newValue != null ? newValue.ToString() : null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex);
+
+            }
+            return null;
+        }
+        /// <summary>
+        /// Get response from xml by tag
+        /// </summary>
+        /// <param name="tagName">tag with response data</param>
+        /// <param name="index">Location of tag in response</param>
+        /// <returns></returns>
+        public object GetValueByTagName(string tagName, int index, int attributeIndex)
+        {
+            try
+            {
+
+                if (objRes != null)
+                {
+                    var newValue = ((dynamic)((objRes.getElementsByTagName(tagName)[index]))).attributes[attributeIndex].text;
+                    return newValue != null ? newValue.ToString() : null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex);
+
+            }
+            return null;
+        }
+        /// <summary>
+        /// Get string error
+        /// </summary>
+        /// <returns></returns>
+        public string ErrorResponse
+        {
+            get
+            {
+                if (!_succes)
+                {
+                    return _response;
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ XmlService/LoginXmlHandler.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LSSERVICEPROVIDERLib;
+using MSXML;
+
+namespace XmlService
+{
+    public class LoginXmlHandler : BaseXmlHandler
+    {
+
+        private IXMLDOMElement tableElem;
+
+
+
+        public LoginXmlHandler(INautilusServiceProvider sp)
+            : base(sp)
+        {
+
+
+        }
+
+        public LoginXmlHandler(INautilusServiceProvider sp, string savedXmlName)
+            : base(sp, savedXmlName)
+        {
+
+        }
+
+        /// <summary>
+        /// Create login xml
+        /// </summary>
+        /// <param name="tableName">Name of entity to login</param>
+        /// <param name="workflowName">Name of workflow to creates entity </param>
+        public void CreateLoginXml(string tableName, string workflowName)
+        {
+
+            objDoc = new DOMDocument();
+
+            //Creates lims request element
+            var objLimsElem = objDoc.createElement("lims-request");
+            objDoc.appendChild(objLimsElem);
+
+            // Creates login request element
+            var objLoginElem = objDoc.createElement("login-request");
+            objLimsElem.appendChild(objLoginElem);
+
+            // Creates tableName element
+            tableElem = objDoc.createElement(tableName);
+            objLoginElem.appendChild(tableElem);
+
+            // Creates create by workflow element
+            var objCreateByWorkflowElem = objDoc.createElement("create-by-workflow");
+            tableElem.appendChild(objCreateByWorkflowElem);
+
+
+            //'creates workflow-name element
+            var objMemberName = objDoc.createElement("workflow-name");
+            objCreateByWorkflowElem.appendChild(objMemberName);
+            objMemberName.text = workflowName;
+
+
+
+        }
+
+        /// <summary>
+        /// Create Login xml under other entity
+        /// </summary>
+        /// <param name="baseTable">Name of parent entity</param>
+        /// <param name="baseObjIdentity">Parent ID</param>
+        /// <param name="childTable">Name of entity to login</param>
+        /// <param name="workFlowName">Name of workflow to creates entity</param>
+        /// <param name="findBy">find by parent</param>
+        public void CreateLoginChildXml(string baseTable, string baseObjIdentity, string childTable, string workFlowName, FindBy findBy)
+        {
+            objDoc = new DOMDocument();
+            var objLimsElem = objDoc.createElement("lims-request");
+            objDoc.appendChild(objLimsElem);
+
+            // Creates login request element
+            var objLoginElem = objDoc.createElement("login-request");
+            objLimsElem.appendChild(objLoginElem);
+
+
+            var entityBaseName = objDoc.createElement(baseTable);
+            objLoginElem.appendChild(entityBaseName);
+
+
+            AddFindByElement(entityBaseName, baseObjIdentity, findBy);
+
+            tableElem = objDoc.createElement(childTable);
+            entityBaseName.appendChild(tableElem);
+
+
+            var createByWfElem = objDoc.createElement("create-by-workflow");
+            tableElem.appendChild(createByWfElem);
+
+            var WFNameElem = objDoc.createElement("workflow-name");
+            createByWfElem.appendChild(WFNameElem);
+            WFNameElem.text = workFlowName;
+
+        }
+
+        /// <summary>
+        /// Add property to login xml
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="propertyValue"></param>
+        public void AddProperties(string propertyName, string propertyValue)
+        {
+
+            AddPropertiesToXml(propertyName, propertyValue);
+        }
+
+        /// <summary>
+        /// Create property element
+        /// </summary>
+        /// <param name="propertyName">Name of property in DB</param>
+        /// <param name="propertyValue">Property value</param>     
+        private void AddPropertiesToXml(string propertyName, string propertyValue)
+        {
+
+            IXMLDOMElement objpropertyName = objDoc.createElement(propertyName);
+            tableElem.appendChild(objpropertyName);
+            objpropertyName.text = propertyValue;
+
+        }
+
+        /// <summary>
+        /// Create find by element
+        /// </summary>
+        /// <param name="entityBaseName"></param>
+        /// <param name="baseObjIdentity"></param>
+        /// <param name="findBy"></param>
+        private void AddFindByElement(IXMLDOMElement entityBaseName, string baseObjIdentity, FindBy findBy)
+        {
+            if (findBy == FindBy.Id)
+            {
+                var findByElem = objDoc.createElement("find-by-id");
+                entityBaseName.appendChild(findByElem);
+                findByElem.text = baseObjIdentity;
+            }
+            else if (findBy == FindBy.Name)
+            {
+                var findByElem = objDoc.createElement("find-by-name");
+                entityBaseName.appendChild(findByElem);
+                findByElem.text = baseObjIdentity;
+            }
+        }
+
+      
+    }
+}
